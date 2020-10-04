e3752c086bdc1fbd88e64dbc810b10b42df8c83e
Jesus Murguia <jesusmurguiafac@gmail.com>
2020-10-04 15:03:00 -0700

    se quito el navbar de app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import './css/App.css';
-import Nfc from './pages/Grafica';
 import Navbar from "./componentes/Navbar";
-import Features from "./pages/GlobalTargets";
-import About from './pages/About';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 
-const NavBar = () => {
+
+const App = () => {
   return(
+    
       <div className="App">
-          <Router>
-          <Navbar className="nav"/>
-
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/Features" component={Features}>
-          <Features />
-        </Route>
-        <Route path="/pages/NetForestCoverChange" component={Nfc}>
-          <Nfc />
-        </Route>
-        <Route path="/About" component={About}>
-          <About />
-        </Route>
-      </Switch>
-  </Router>
+          <Navbar/>
       </div>
   )
 }
-export default NavBar;
+export default App;
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -3,15 +3,24 @@ import "../css/App.css"
 import * as ReactBootStrap from "react-bootstrap";
 import {
     BrowserRouter as Router,
+    Switch,
+    Route,
     Link
 } from 'react-router-dom';
+import Nfc from '../pages/Grafica';
 
+import Features from "../pages/GlobalTargets";
+import About from '../pages/About';
 
 
 const NavBar = () => {
     return(
-        <div className="App">
-    <ReactBootStrap.Navbar collapseOnSelect expand="xl" className="nav" variant="light">
+        <div className="NavBar">
+
+<Router>
+
+
+<ReactBootStrap.Navbar collapseOnSelect expand="xl" className="nav" variant="light">
     <Link to="/pages/NetForestCoverChange">
       
   <ReactBootStrap.Navbar.Brand href="#home">Scenathon 2020</ReactBootStrap.Navbar.Brand>
@@ -29,6 +38,25 @@ const NavBar = () => {
     </ReactBootStrap.Nav>
   </ReactBootStrap.Navbar.Collapse>
 </ReactBootStrap.Navbar>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/Features" component={Features}>
+          <Features />
+        </Route>
+        <Route path="/pages/NetForestCoverChange" component={Nfc}>
+          <Nfc />
+        </Route>
+        <Route path="/About" component={About}>
+          <About />
+        </Route>
+      </Switch>
+ 
+
+
+
+</Router>
         </div>
     )
 }
