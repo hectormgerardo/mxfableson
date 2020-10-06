0863ffcc89df4caee9cc8466feb40ea368856afc
AlejandraCeniceros <dcac@outlook.es>
2020-10-06 03:47:39 -0700

    no es mucho pero es trabajo honesto

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Navbar from "./componentes/Navbar";
 import About from './pages/About';
 import {Jumbotron } from './componentes/Jumbotron'
 import { Jumbotron_2} from './componentes/Jumbotron_2'
+import { Jumbotron_3} from './componentes/Jumbotron_3'
+import { Jumbotron_fin} from './componentes/Jumbotron_fin'
+
 
 
 class App extends Component {
   render(){
     return (
     <React.Fragment>
-           <Jumbotron/> <Navbar/>
-          <About/>
-          <Jumbotron_2/>
+      <Jumbotron/> <Navbar/>
+        <About/>
+        <Jumbotron_2/>
+        <Jumbotron_3/>
+        <Jumbotron_fin/>
          
     </React.Fragment>
   )
--- src/componentes/Jumbotron_fin.js
+++ src/componentes/Jumbotron_fin.js
@@ -1,41 +1,44 @@
-  
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
-import Banner2 from '../assets/banner_2.jpg';
 
 const Styles = styled.div`
-   
-    .jumbo2{
-        background: url(${Banner2});
+  
+    }
+    .fin_t{
+        background-color: #528D93;
+
         background-size: cover;
         color: #ccc;
-        height: 500px;
-        position: absoulte;
+        height: 20px;
+        position: relative;
         z-index: -2;
     }
+    .fin{
+      text-align:center;
+      color: #000000;
+    }
     .overlay{
-        background-color: #000;
+        background-color: #528D93;
         opacity:0.6;
         position: absolute;
-        top:0;
+        top:0; 
         left: o;
-        bottom: -5;
+        bottom: 5;
         right: 0;
         z-index: -1;
     }`;
-
-
-export const Jumbotron_2 = () => {
-    return(
-
+export const Jumbotron_fin = () => {
+  return (
     <Styles>
-        <Jumbo fluid className= "jumbo2">
+        <Jumbo fluid className= "fin_t">
             <div className= "overlay"></div>
             <Container>
-               
+            <li className="fin">Copyright ©</li>
+
             </Container>
         </Jumbo>
     </Styles>
-    )
+    
+  );
 };
