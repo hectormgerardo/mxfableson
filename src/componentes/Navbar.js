498ad9c78eab7e657699eea55ae85409f30828c5
Francisco Javier Barreras Cebreros <francisco.barreras203706@potros.itson.edu.mx>
2020-10-13 02:37:58 -0700

    NavBar Actualizado

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react';
 import "../css/App.css"
 import * as ReactBootStrap from "react-bootstrap";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
 } from 'react-router-dom';
-import Nfc from '../pages/Scenathon';
+import Scenathon from '../pages/Scenathon';
 import Features from "../pages/GlobalTargets";
 import About from '../pages/About';
 import styled from 'styled-components';
@@ -25,53 +25,54 @@ const Styles = styled.div`
 }
 `;
 const NavBar = () => {
-    return(
+  return (
 
-      <Styles>
-        <div className="NavBar">
+    <Styles>
+      <div className="NavBar">
 
-          <Router>
+        <Router>
 
 
-            <ReactBootStrap.Navbar collapseOnSelect expand="xl" className="nav" variant="light">
-                <Link to="/">
+          <ReactBootStrap.Navbar collapseOnSelect expand="xl" className="nav" variant="light">
+            <Link to="/">
 
               <ReactBootStrap.Navbar.Brand href="#home">Fable</ReactBootStrap.Navbar.Brand>
-              </Link>
-              <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-                <ReactBootStrap.Nav className="mr-auto"> 
-
-
-                </ReactBootStrap.Nav>
-                <ReactBootStrap.Nav>
-                <ReactBootStrap.Nav.Link href="#About">About </ReactBootStrap.Nav.Link>
-                </ReactBootStrap.Nav>
-              </ReactBootStrap.Navbar.Collapse>
-            </ReactBootStrap.Navbar>
-
-                  {/* A <Switch> looks through its children <Route>s and
-                      renders the first one that matches the current URL. */}
-                  <Switch>
-                    <Route path="/Features" component={Features}>
-                      <Features />
-                    </Route>
-                    <Route path="/" component={Nfc}>
-                      <Nfc />
-                    </Route>
-                    <Route path="/About"  render={
-                      ()=>{
-                        return( <h1>About</h1> );
-                      }
-                    }>
-                      <About/>
-                    </Route>
-                  </Switch>
+            </Link>
           
-          </Router>
-        </div>
-      </Styles>
-    )
+            <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+              <ReactBootStrap.Nav className="mr-auto">
+
+
+              </ReactBootStrap.Nav>
+              <ReactBootStrap.Nav>
+                <ReactBootStrap.Nav.Link href="#About">About </ReactBootStrap.Nav.Link>
+              </ReactBootStrap.Nav>
+            </ReactBootStrap.Navbar.Collapse>
+          </ReactBootStrap.Navbar>
+
+          {/* A <Switch> looks through its children <Route>s and
+                      renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/Features" component={Features}>
+              <Features />
+            </Route>
+            <Route path="/" component={Scenathon}>
+              <Scenathon />
+            </Route>
+            <Route path="/About" render={
+              () => {
+                return (<h1>About</h1>);
+              }
+            }>
+              <About />
+            </Route>
+          </Switch>
+
+        </Router>
+      </div>
+    </Styles>
+  )
 }
 
 export default NavBar;
