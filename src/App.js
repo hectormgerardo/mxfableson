bedd3d46c954f83c25c49e56df36640e2a9e63c0
Carlos Encinas <carlos-encinas@hotmail.com>
2020-10-14 22:22:14 -0700

    Add tour without steps

--- src/App.js
+++ src/App.js
@@ -6,17 +6,53 @@ import { Jumbotron } from './componentes/Jumbotron'
 import { Jumbotron_2} from './componentes/Jumbotron_2'
 import { Jumbotron_3} from './componentes/Jumbotron_3'
 import { Jumbotron_fin} from './componentes/Jumbotron_fin'
+import Joyride from 'react-joyride';
+import { Last } from 'react-bootstrap/esm/PageItem';
 
 
-class App extends Component {  
+export class App extends React.Component {
+  state = {
+    steps: [
+      {
+        target: '.About',
+        content: 'En este apartado se hablará a líneas generales acerca de lo que es Fable',
+      },
+      {
+        target: '.Nav',
+        content: 'Seccion uno de menu',
+      }
+    ]
+  };
+  
   render(){
+    const { steps } = this.state;
     return (
     <React.Fragment>
       <Jumbotron/> 
-      <Navbar/>
+      <Navbar id="Nav" />
         <div id="About">
              <About/>
         </div>
+        <div className="app">
+        <Joyride
+          steps={steps}
+          continuous={true}
+          showSkipButton={true}
+          
+          styles={{
+            tooltipContainer: {
+              textAlign: "left"
+            },
+            buttonNext: {
+              backgroundColor: "green"
+            },
+            buttonBack: {
+              marginRight: 10
+            }
+          }}
+        />
+      </div>
+
         <Jumbotron_2/>
         <Jumbotron_3/>
         <Jumbotron_fin/>
