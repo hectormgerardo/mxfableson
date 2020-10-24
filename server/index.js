d1f6f9fdcfdd1c2e27658037d58cfb7034ab5d90
Jesus Murguia <jesusmurguiafac@gmail.com>
2020-10-24 15:25:36 -0700

    added Protected Areas by Type query

--- server/index.js
+++ server/index.js
@@ -42,5 +42,35 @@ console.error(err.message);
 }
 });
 
+app.get('/protected:combinaciones',async(req,res)=>{
+    try{
+        
+
+        const{iteration,group}=JSON.parse(req.params.combinaciones).select;
+
+        switch(group){
+            case "group": 
+            var query="SELECT \"Year\", SUM(\"ProtectedAreasForest\") AS \"ProtectedAreasForest\", SUM(\"ProtectedAreasOther\") AS \"ProtectedAreasOther\", SUM(\"ProtectedAreasOtherNat\") AS \"ProtectedAreasOtherNat\" FROM \"resultsScen2020\" WHERE \"iteration\"=$1 GROUP BY \"Year\" ORDER BY \"Year\"";
+                break;
+            case "countries":
+                var query="SELECT \"Year\", SUM(\"ProtectedAreasForest\") AS \"ProtectedAreasForest\", SUM(\"ProtectedAreasOther\") AS \"ProtectedAreasOther\", SUM(\"ProtectedAreasOtherNat\") AS \"ProtectedAreasOtherNat\" FROM \"resultsScen2020\" WHERE \"iteration\"=$1 AND \"Country\" NOT LIKE '%$_%' ESCAPE '$'  GROUP BY \"Year\" ORDER BY \"Year\"";
+                break;
+            case "regions":
+                var query="SELECT \"Year\", SUM(\"ProtectedAreasForest\") AS \"ProtectedAreasForest\", SUM(\"ProtectedAreasOther\") AS \"ProtectedAreasOther\", SUM(\"ProtectedAreasOtherNat\") AS \"ProtectedAreasOtherNat\" FROM \"resultsScen2020\" WHERE \"iteration\"=$1 AND \"Country\" LIKE '%$_%' ESCAPE '$'  GROUP BY \"Year\" ORDER BY \"Year\"";
+                break;
+            default: 
+                var query=null;
+                break;
+        }
+        const response=await pool.query(query,[iteration]);
+
+res.status(200).json(response.rows)
+
+
+
+}catch(err){
+console.error(err.message);
+}
+});
 
 
