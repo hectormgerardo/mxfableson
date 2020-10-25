89a8474569eb8db304a16cef64d8fb0ef35301f1
Jesus Murguia <jesusmurguiafac@gmail.com>
2020-10-24 21:53:05 -0700

    food energy 1 and 2 and land cover queries

--- server/index.js
+++ server/index.js
@@ -158,3 +158,61 @@ console.error(err.message);
 }
 });
 
+app.get('/foodenergy2:combinaciones',async(req,res)=>{
+    try{
+    const{iteration,year}=JSON.parse(req.params.combinaciones).select;
+        var query='Select "Country",avg("prot_feas")as "Protein_feasible",avg("fat_feas") as "Fat_feasible" from "resultsScen2020" WHERE "iteration"=$1 AND "Year"=$2 GROUP BY "Country"';
+        
+const response=await pool.query(query,[iteration,year]);
+
+res.status(200).json(response.rows)
+
+
+
+}catch(err){
+console.error(err.message);
+}
+});
+app.get('/foodenergy1:combinaciones',async(req,res)=>{
+    try{
+    const{iteration,scenathon,year}=JSON.parse(req.params.combinaciones).select;
+        var query='SELECT "Country", (avg("kcal_feas")) AS Kcal_feasible, avg("kcal_mder") AS Target_MDER FROM "resultsScen2020" WHERE "iteration" = $1 AND "scenathon_id" = $2 AND "Year" = $3 GROUP BY "Country" ORDER BY "Country";';
+        
+const response=await pool.query(query,[iteration,scenathon,year]);
+
+res.status(200).json(response.rows)
+
+
+
+}catch(err){
+console.error(err.message);
+}
+});
+
+app.get('/landcover',async(req,res)=>{
+    try{
+    const{iteration,scenathon,group}=JSON.parse(req.params.combinaciones).select;
+    switch(group){
+        case "group": 
+        var query='SELECT "Year",sum("CalcPasture") as "CalcPasture",sum("CalcCropland") as "CalcCropland",sum("CalcForest") as "CalcForest",sum("CalcNewForest") as "CalcNewForest" ,sum("CalcOtherLand") as "CalcOtherLand",sum("CalcUrban") as "CalcUrban" from "resultsScen2020" WHERE "iteration"=$1 GROUP BY "Year" order by "Year"';
+        break;
+        case "countries":
+            var query='SELECT "Year",sum("CalcPasture") as "CalcPasture",sum("CalcCropland") as "CalcCropland",sum("CalcForest") as "CalcForest",sum("CalcNewForest") as "CalcNewForest" ,sum("CalcOtherLand") as "CalcOtherLand",sum("CalcUrban") as "CalcUrban" from "resultsScen2020" WHERE "iteration"=$1 AND "Country" NOT LIKE \'%$_%\' ESCAPE \'$\' GROUP BY "Year" order by "Year"';
+            break;
+        case "regions":
+            var query='SELECT "Year",sum("CalcPasture") as "CalcPasture",sum("CalcCropland") as "CalcCropland",sum("CalcForest") as "CalcForest",sum("CalcNewForest") as "CalcNewForest" ,sum("CalcOtherLand") as "CalcOtherLand",sum("CalcUrban") as "CalcUrban" from "resultsScen2020" WHERE "iteration"=$1 AND "Country" LIKE \'%$_%\' ESCAPE \'$\' GROUP BY "Year" order by "Year"';
+            break;
+        default: 
+            var query=null;
+            break;
+    }
+const response=await pool.query(query,[iteration]);
+
+res.status(200).json(response.rows)
+
+
+
+}catch(err){
+console.error(err.message);
+}
+});
